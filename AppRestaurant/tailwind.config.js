94726ee181ef06fcb1ca9878c86506fb9238751b
Uriel MV Linux <bustechsys@gmail.com>
2023-11-02 13:54:45 -0600

    Tailwindcss en React

---
+++ AppRestaurant/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    //Explica en que archivos abra codigo de tailwindcss
+    "./index.html",
+    "./src/**/*.{js,jsx}" //todo lo que este en src y sean JS JSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
