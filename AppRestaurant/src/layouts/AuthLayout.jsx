04d438ec398588e626ac23b1ac5ef7e8062d5e9c
Uriel MV Linux <bustechsys@gmail.com>
2023-11-03 23:51:49 -0600

    Html para Crear cuenta

--- AppRestaurant/src/layouts/AuthLayout.jsx
+++ AppRestaurant/src/layouts/AuthLayout.jsx
@@ -2,9 +2,13 @@ import { Outlet } from "react-router-dom"
 
 export default function AuthLayout() {
   return (
-    <div>
-        AuthLayout
+    <main className="max-w-4xl m-auto md:mt-28 flex flex-col md:flex-row items-center">
+      <img src="../img/logo.svg" alt="Imagen logo" className="max-w-xs" />
+      <div className="p-10 w-full">
+        
         <Outlet />
-    </div>
+      </div>
+    </main>
+    
   )
 }
